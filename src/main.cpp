6ba23355541f1e5f26e590b4d9432c56270b81f2
Johnna Louie <johnnalouie@gmail.com>
2025-11-24 14:50:11 -0800

    Initial setup of PIR sensor and servo

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,41 @@
 #include <Arduino.h>
+#include <Servo.h>
 
-// put function declarations here:
-int myFunction(int, int);
+#define PIR_PIN 21
+#define SERVO_PIN 22
+
+unsigned long stableTime = 1000;
+bool motionDetected = false;
+
+Servo myServo;
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial.begin(9600);
+  pinMode(PIR_PIN, INPUT);
+  myServo.attach(SERVO_PIN);
+  myServo.write(0);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  int reading = digitalRead(PIR_PIN);
+
+  // If PIR goes HIGH, double-check it after a delay
+  if (reading == HIGH) {
+    delay(stableTime);
+    if (digitalRead(PIR_PIN) == HIGH && !motionDetected) {
+      motionDetected = true;
+      Serial.println("Motion CONFIRMED");
+      myServo.write(90);
+      delay(500);
+      myServo.write(0);
+    }
+  }
+
+  // If PIR goes LOW, reset state
+  if (reading == LOW && motionDetected) {
+    motionDetected = false;
+    Serial.println("Motion ended");
+  }
+  delay(200);
 }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
-}
